b9915c5207f6f27e4f8cc07282a373cfd9910e88
RJA <jshrykes@gmail.com>
2024-02-07 11:05:55 -0500

    Added remaining connections for INewsRepository

--- app/src/main/java/com/jetpack/newsapidemo/domain/usecase/DeleteSavedNewsUseCase.kt
+++ app/src/main/java/com/jetpack/newsapidemo/domain/usecase/DeleteSavedNewsUseCase.kt
@@ -1,6 +1,10 @@
 package com.jetpack.newsapidemo.domain.usecase
 
+import com.jetpack.newsapidemo.data.Article
 import com.jetpack.newsapidemo.domain.repository.INewsRepository
 
 class DeleteSavedNewsUseCase(private val newsRepository: INewsRepository) {
+
+    suspend fun execute(article: Article) = newsRepository.deleteNews(article)
+
 }
--- app/src/main/java/com/jetpack/newsapidemo/domain/usecase/GetSavedNewsUseCase.kt
+++ app/src/main/java/com/jetpack/newsapidemo/domain/usecase/GetSavedNewsUseCase.kt
@@ -1,6 +1,12 @@
 package com.jetpack.newsapidemo.domain.usecase
 
+import com.jetpack.newsapidemo.data.Article
 import com.jetpack.newsapidemo.domain.repository.INewsRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetSavedNewsUseCase(private val newsRepository: INewsRepository) {
+
+    fun execute() : Flow<List<Article>>{
+        return  newsRepository.getSavedNews()
+    }
 }
