e8290f3973664ca52ef766ed432c818fc4d4c6d0
RJA <jshrykes@gmail.com>
2024-02-07 12:44:50 -0500

    Created function to handle API response returning Resource<APIResponse> type   - best practices for handling http responses suuccess/error/loading

--- app/src/main/java/com/jetpack/newsapidemo/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/jetpack/newsapidemo/data/repository/NewsRepositoryImpl.kt
@@ -2,15 +2,29 @@ package com.jetpack.newsapidemo.data.repository
 
 import com.jetpack.newsapidemo.data.APIResponse
 import com.jetpack.newsapidemo.data.Article
+import com.jetpack.newsapidemo.data.repository.dataSource.INewsRemoteDataSource
 import com.jetpack.newsapidemo.data.util.Resource
 import com.jetpack.newsapidemo.domain.repository.INewsRepository
 import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
 
-class NewsRepositoryImpl: INewsRepository {
+class NewsRepositoryImpl(
+    private val newsRemoteDataSource: INewsRemoteDataSource,
+
+): INewsRepository {
     override suspend fun getNewsHeadlines(): Resource<APIResponse> {
         TODO("Not yet implemented")
     }
 
+    private fun responseToResource(response: Response<APIResponse>):Resource<APIResponse> {
+        if(response.isSuccessful){
+            response.body()?.let { result ->
+                return Resource.Success(result)
+            }
+        }
+
+        return Resource.Error(response.message())
+    }
     override suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse> {
         TODO("Not yet implemented")
     }
