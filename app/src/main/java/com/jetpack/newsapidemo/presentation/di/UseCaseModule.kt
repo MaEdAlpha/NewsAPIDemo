4c24afbf305fd42472237dbf1bc91fafcbfeafd4
RJA <jshrykes@gmail.com>
2024-02-07 16:06:16 -0500

    add UseCaseModule

---
+++ app/src/main/java/com/jetpack/newsapidemo/presentation/di/UseCaseModule.kt
@@ -0,0 +1,21 @@
+package com.jetpack.newsapidemo.presentation.di
+
+import com.jetpack.newsapidemo.domain.repository.INewsRepository
+import com.jetpack.newsapidemo.domain.usecase.GetNewsHeadlinesUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class UseCaseModule {
+    @Provides
+    fun provideNewsHeadLinesUseCase(
+        newsRepository: INewsRepository
+    ) : GetNewsHeadlinesUseCase{
+        //construct and return dependency
+        return GetNewsHeadlinesUseCase(newsRepository)
+    }
+}
