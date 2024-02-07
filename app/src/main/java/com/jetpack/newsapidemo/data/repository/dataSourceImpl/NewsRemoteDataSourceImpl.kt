d6ae97292d0da5840db089c328eca8ca84305cac
RJA <jshrykes@gmail.com>
2024-02-07 12:34:10 -0500

    Pass query params plus NewsAPIService  and call getTopHeadlines()

--- app/src/main/java/com/jetpack/newsapidemo/data/repository/dataSourceImpl/NewsRemoteDataSourceImpl.kt
+++ app/src/main/java/com/jetpack/newsapidemo/data/repository/dataSourceImpl/NewsRemoteDataSourceImpl.kt
@@ -1,4 +1,16 @@
 package com.jetpack.newsapidemo.data.repository.dataSourceImpl
 
-class NewsRemoteDataSourceImpl {
+import com.jetpack.newsapidemo.data.APIResponse
+import com.jetpack.newsapidemo.data.api.INewsAPIService
+import com.jetpack.newsapidemo.data.repository.dataSource.INewsRemoteDataSource
+import retrofit2.Response
+
+class NewsRemoteDataSourceImpl(
+    private val newsAPIService: INewsAPIService,
+    private val country: String,
+    private val page: Int,
+) : INewsRemoteDataSource {
+    override suspend fun getTopHeadliners(): Response<APIResponse> {
+        return newsAPIService.getTopHeadlines(country, page)
+    }
 }
