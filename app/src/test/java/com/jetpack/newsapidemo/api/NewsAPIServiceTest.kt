cc407e6b4920c07cac7079b4de4d0df24568ea5b
RJA <jshrykes@gmail.com>
2024-02-07 14:01:29 -0500

    Link json data to MockServer via MockResponse() and enqueueMockResponse()

--- app/src/test/java/com/jetpack/newsapidemo/api/NewsAPIServiceTest.kt
+++ app/src/test/java/com/jetpack/newsapidemo/api/NewsAPIServiceTest.kt
@@ -1,7 +1,10 @@
 package com.jetpack.newsapidemo.api
 
 import com.jetpack.newsapidemo.data.api.INewsAPIService
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
 import org.junit.After
 import org.junit.Before
 import retrofit2.Retrofit
@@ -20,6 +23,15 @@ class NewsAPIServiceTest {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(INewsAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(fileName: String){
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+
     }
 
     @After //runs after @Test
