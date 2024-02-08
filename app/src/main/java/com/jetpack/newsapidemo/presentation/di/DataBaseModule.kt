a88badcf47137a32997b926d935eaac7d6e2aaeb
RJA <jshrykes@gmail.com>
2024-02-08 15:49:21 -0500

    Added DataBase Module Singelton providers

--- app/src/main/java/com/jetpack/newsapidemo/presentation/di/DataBaseModule.kt
+++ app/src/main/java/com/jetpack/newsapidemo/presentation/di/DataBaseModule.kt
@@ -1,11 +1,31 @@
 package com.jetpack.newsapidemo.presentation.di
 
+import android.app.Application
+import androidx.room.Room
+import com.jetpack.newsapidemo.data.db.ArticleDatabase
+import com.jetpack.newsapidemo.data.db.dao.ArticleDAO
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataBaseModule {
+    @Singleton
+    @Provides
+    fun providerNewsDataBase(app: Application): ArticleDatabase{
+        return Room.databaseBuilder(app, ArticleDatabase::class.java, "news_db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideNewsDao(articleDatabase: ArticleDatabase) : ArticleDAO {
+        return articleDatabase.getArticleDAO()
+    }
+
 
 }
