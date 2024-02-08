607552c17f4e14c9bad23a34f0d8610969a49ea9
RJA <jshrykes@gmail.com>
2024-02-08 14:07:03 -0500

    Update useCase's execute() with correct params

--- app/src/main/java/com/jetpack/newsapidemo/domain/usecase/GetSearchedNewsUseCase.kt
+++ app/src/main/java/com/jetpack/newsapidemo/domain/usecase/GetSearchedNewsUseCase.kt
@@ -5,7 +5,7 @@ import com.jetpack.newsapidemo.data.util.Resource
 import com.jetpack.newsapidemo.domain.repository.INewsRepository
 
 class GetSearchedNewsUseCase(private val newsRepository: INewsRepository) {
-    suspend fun execute(searchQuery: String) : Resource<APIResponse> {
-        return newsRepository.getSearchedNews(searchQuery)
+    suspend fun execute(country: String, searchQuery: String, page: Int) : Resource<APIResponse> {
+        return newsRepository.getSearchedNews(country, searchQuery, page)
     }
 }
